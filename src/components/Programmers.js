2bd2e9c0ea2dbefeb3b504eae4cd88c708c5f686
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-09-07 18:16:33 -0700

    Complete

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -29,7 +29,9 @@ export default function Programmers() {
   // The programmers list on the one hand, and the id of the featured programmer on the other.
 
 const [listNames,listChanger]=useState(listOfAwesome)
-const [Name,nameChanger]=useState(null)
+const [name,nameChanger]=useState(null)
+
+console.log(name)
 
 
   const getNameOfFeatured = () => {
@@ -38,12 +40,13 @@ const [Name,nameChanger]=useState(null)
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
+    return name;
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 color turns to gold, when celebrating
+    color: name!=null ? 'gold' : 'royalblue' // 🤔 color turns to gold, when celebrating
   };
 
   return (
@@ -54,9 +57,9 @@ const [Name,nameChanger]=useState(null)
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          listOfAwesome.map(dev =>
+          listNames.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => { nameChanger(dev.name) /* in here set the featured id to be dev.id */ }}>Feature</button>
             </div>
           )
         }
@@ -66,7 +69,7 @@ const [Name,nameChanger]=useState(null)
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
+          name
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
